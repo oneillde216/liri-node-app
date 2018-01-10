d3cd7b78daf25f3e526acda07e72a7c4b25b8bf4
oneillde216 <31212974+oneillde216@users.noreply.github.com>
2018-01-10 16:46:32 -0500

    Add files via upload

--- liri.js
+++ liri.js
@@ -58,10 +58,10 @@ function dylanlikesmovies(){
 	        console.log("Title: " + JSON.parse(body)["Title"]);
 	        console.log("Year: " + JSON.parse(body)["Year"]);
 	        console.log("IMDB Rating: " + JSON.parse(body)["imdbRating"]);
+	        console.log("Rotten Tomatoes Rating: " + JSON.parse(body)["tomatoRating"]);
 	        console.log("Country: " + JSON.parse(body)["Country"]);
 	        console.log("Language: " + JSON.parse(body)["Language"]);
 	        console.log("Plot: " + JSON.parse(body)["Plot"]);
 	        console.log("Actors: " + JSON.parse(body)["Actors"]);
-	        console.log("Rotten Tomatoes Rating: " + JSON.parse(body)["tomatoRating"]);
-	        console.log("Rotten Tomatoes URL: " + JSON.parse(body)["tomatoURL"]);
+	        
 };
